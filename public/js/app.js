2a1c2139a4d3d5b542d78bec9c3a6f80e4783755
kevin jose <kevin.jose@aot-technologies.com>
2015-08-10 13:16:17 +0530

    refactored for food bowl

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,10 @@
  */
 angular
     .module('MuppetApp', ['ngMaterial', 'ui.router'])
+
+    .run(['$rootScope', function ($rootScope) {
+        $rootScope.title = "Food-Bowl";
+    }])
     /*Ui router configuration*/
     .config(
     function ($stateProvider, $urlRouterProvider) {
@@ -46,7 +50,7 @@ angular
     .config(function ($mdThemingProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('deep-orange')
-            .accentPalette('brown')
+            .accentPalette('red')
             .warnPalette('blue-grey');
     })
     /*Landing page controller*/
@@ -60,6 +64,51 @@ angular
             'Indian',
             'Chinese'
         ];
+        $scope.restaurants = [
+            {
+                name: "Dominos",
+                imgurl: "http://static.in.groupon-content.net/64/68/1321011396864.jpg",
+                menuContent: [
+                    {
+                        categoryName: "Regular (serves 1)",
+                        food: [
+                            {
+                                name: "Margherita",
+                                description: "A hugely popular margherita, with a deliciously tangy single cheese topping."
+                            },
+                            {
+                                name: "Country Special",
+                                description: "For all those with a partiality for veggies, this one's loaded - crunchy onions, crisp capsicum and fresh juicy tomatoes. Yum!"
+                            }
+                        ]
+                    }, {
+                        categoryName: "Medium (serves 2)",
+                        food: [
+                            {
+                                name: "Chicken Salami Special",
+                                description: "Who doesn't love some cheese and Salami? An all time winning combination of exotic, seasoned chicken salami and mouthwatering cheese. What's not to love right?"
+                            },
+                            {
+                                name: "Peppy Paneer",
+                                description: "Chunky paneer with crisp capsicum and spicy red pepper - quite a mouthful!"
+                            }
+                        ]
+                    }, {
+                        categoryName: "Large (serves 4)",
+                        food: [
+                            {
+                                name: "Mexican Green Wave",
+                                description: "A pizza loaded with crunchy onions, crisp capsicum, juicy tomatoes and jalapeno with a liberal sprinkling of exotic Mexican herbs"
+                            },
+                            {
+                                name: "Bbq Chicken ",
+                                description: "A flavour of barbeque chicken spiked with onions"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
         $scope.click = function () {
             console.log("Clicked");
         }
